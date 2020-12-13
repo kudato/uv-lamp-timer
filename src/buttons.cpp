eb34dfe3267d87f0e4f4c074ee62a64cea39fd94
Alexander Shevchenko <kudato@me.com>
2020-12-13 05:43:11 +0300

    Some refactoring.

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -1,88 +1,74 @@
 #pragma once
 #include "buttons.h"
 #include "buzzer.h"
-#include <Arduino.h>
 
+//--------------------------------------------------------------------
+// Variables
+//--------------------------------------------------------------------
+
+// State variables
 static volatile bool OK_PRESSED = false;
 static volatile bool UP_PRESSED = false;
 static volatile bool DOWN_PRESSED = false;
 
+// Pressing time for soft debouncing
 static volatile uint32_t OK_BUTTON_DEBOUNCE_TIME = 0;
 static volatile uint32_t UP_BUTTON_DEBOUNCE_TIME = 0;
 static volatile uint32_t DOWN_BUTTON_DEBOUNCE_TIME = 0;
 
+//--------------------------------------------------------------------
+// Button events handling
+//--------------------------------------------------------------------
 
+// Pin change interrupt
 ISR (PCINT1_vect) {
-    if (((millis()-OK_BUTTON_DEBOUNCE_TIME) > DEBOUNCE_DELAY)
-       && (Button::okButtonState() == true))
+    if (((millis()-OK_BUTTON_DEBOUNCE_TIME) > BUTTON_DEBOUNCE_DELAY)
+       && (Button::buttonState(OK_BUTTON_PIN) == true))
     {
         OK_BUTTON_DEBOUNCE_TIME = millis();
         OK_PRESSED = true;
     }
 
-    if (((millis()-UP_BUTTON_DEBOUNCE_TIME) > DEBOUNCE_DELAY)
-       && (Button::upButtonState() == true))
+    if (((millis()-UP_BUTTON_DEBOUNCE_TIME) > BUTTON_DEBOUNCE_DELAY)
+       && (Button::buttonState(UP_BUTTON_PIN) == true))
     {
         UP_BUTTON_DEBOUNCE_TIME = millis();
         UP_PRESSED = true;
     }
 
-    if (((millis()-DOWN_BUTTON_DEBOUNCE_TIME) > DEBOUNCE_DELAY)
-       && (Button::downButtonState() == true))
+    if (((millis()-DOWN_BUTTON_DEBOUNCE_TIME) > BUTTON_DEBOUNCE_DELAY)
+       && (Button::buttonState(DOWN_BUTTON_PIN) == true))
     {
         DOWN_BUTTON_DEBOUNCE_TIME = millis();
         DOWN_PRESSED = true;
     }
 }
 
-
-bool Button::okButtonState(void) {
-    if ((PINC & (1 << OK_BUTTON_PIN)) == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-bool Button::upButtonState(void)  {
-    if ((PINC & (1 << UP_BUTTON_PIN)) == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-bool Button::downButtonState(void)  {
-    if ((PINC & (1 << DOWN_BUTTON_PIN)) == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
+//--------------------------------------------------------------------
+// Setup pins and pin change interrupt
+//--------------------------------------------------------------------
 
 void Button::begin(void)
 {
     // enable INPUT on A0-A2 pins of the board
-    DDRC &= ~(
-        (1 << DDC2) |
-        (1 << DDC1) |
-        (1 << DDC0)
-    );
-    // add PULL_UP on A0-A2 pins of board
-    PORTC |= (
-        (1 << DDC2) |
-        (1 << DDC1) |
-        (1 << DDC0)
-    );
+    DDRC &= ~((1 << DDC2) | (1 << DDC1) | (1 << DDC0));
+
+    // add PULL_UP on A0-A2 pins of the board
+    PORTC |= ((1 << DDC2) | (1 << DDC1) | (1 << DDC0));
+
     // enable PCIE1 interrupts
     PCICR |= (1 << PCIE1);
+
     // configure interrupts on A0,A1,A2 pins only
     PCMSK1 |= ((1 << PCINT10) | (1 << PCINT9) | (1 << PCINT8));
+
     // enable interrupts
     interrupts();
 }
 
+//--------------------------------------------------------------------
+// Button events handler static class
+//--------------------------------------------------------------------
 
 uint8_t Button::pressed(void)
 {
@@ -105,4 +91,13 @@ uint8_t Button::pressed(void)
         return OK_BUTTON;
     }
     return 0;
+}
+
+
+bool Button::buttonState(uint8_t button_pin) {
+    if ((PINC & (1 << button_pin)) == 0)
+    {
+        return true;
+    }
+    return false;
 }
--- src/buttons.h
+++ src/buttons.h
@@ -1,25 +1,19 @@
 #pragma once
-#include <stdint.h>
+#include "../uv-lamp-timer.h"
 
-#define OK_BUTTON 1
-#define UP_BUTTON 2
-#define DOWN_BUTTON 3
-
+// Button pins
 #define OK_BUTTON_PIN PINC0
 #define UP_BUTTON_PIN PINC1
 #define DOWN_BUTTON_PIN PINC2
 
-// This value if you use large metal buttons designed for high voltage,
-// if you have tactile button, this value can probably be reduced to 25-50
-#define DEBOUNCE_DELAY 250
-
+//--------------------------------------------------------------------
+// Button events handler static class
+//--------------------------------------------------------------------
 
 class Button
 {
     public:
         static void begin(void);
         static uint8_t pressed(void);
-        static bool okButtonState(void);
-        static bool upButtonState(void);
-        static bool downButtonState(void);
+        static bool buttonState(uint8_t);
 };
